8487b5b0ad1829a4615e48c84038a1c2b6182745
Gaurav Prajapati <76619300+gauravprajapati007@users.noreply.github.com>
2024-02-18 12:34:19 +0530

    Add files via upload

---
+++ src/com/ty/Id.java
@@ -0,0 +1,13 @@
+package com.ty;
+
+import java.util.Comparator;
+
+public class Id implements Comparator<Book> {
+
+	@Override
+	public int compare(Book o1, Book o2) {
+
+		return o1.getId().compareTo(o2.getId());
+	}
+
+}
